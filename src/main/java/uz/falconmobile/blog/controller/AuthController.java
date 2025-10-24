164914215d50a529d4cd8b03e87211b38f34d0b5
yengilmas <lbek458@gmail.com>
2025-10-25 01:24:36 +0500

    work started , entitys created Category lar listini to'liq chiqarish qismi bajarildi category 100% bajarildi testlar yozildi SecurityConfig file ustida ishlanyabdi bazi yangilanishlar qilindi AuthController.java va authservice bilan bazi ishlar qilindi

--- src/main/java/uz/falconmobile/blog/controller/AuthController.java
+++ src/main/java/uz/falconmobile/blog/controller/AuthController.java
@@ -4,6 +4,8 @@ package uz.falconmobile.blog.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,13 +17,21 @@ import uz.falconmobile.blog.services.AuthanticationService;
 @RequestMapping("v1/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
-//
-//    private final AuthanticationService  authanticationService;
-//
-//    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest loginRequest) {
-//
-//        authanticationService.authenticate()
-//
-//    }
+
+    private final AuthanticationService  authanticationService;
+
+    @PostMapping
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest loginRequest) {
+
+        UserDetails userDetails = authanticationService.authenticate(loginRequest.getEmail(), loginRequest.getPassword());
+        String token = authanticationService.generateToken(userDetails);
+        AuthResponse authResponse = AuthResponse.builder()
+                .token(token)
+                .expiresIn(86400L)
+                .build();
+
+        return ResponseEntity.ok(authResponse);
+
+    }
 
 }
--- src/main/java/uz/falconmobile/blog/services/AuthanticationService.java
+++ src/main/java/uz/falconmobile/blog/services/AuthanticationService.java
@@ -3,7 +3,9 @@ package uz.falconmobile.blog.services;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface AuthanticationService {
 
     UserDetails authenticate(String username, String password);
